3ab2297c5601304cf5caac8cca0be492dcc2e76d
Thế Anh Hoàng <anhhtchy@gmail.com>
2019-11-14 21:08:34 +0700

    add menu

--- sources/desktop/src/com/simplerpg/game/desktop/DesktopLauncher.java
+++ sources/desktop/src/com/simplerpg/game/desktop/DesktopLauncher.java
@@ -3,10 +3,11 @@ package com.simplerpg.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.simplerpg.game.SimpleRPG;
+import com.simplerpg.game.SimpleRPGGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main (String[] args) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new SimpleRPG(), config);
+		new LwjglApplication(new SimpleRPGGame(), config);
 	}
 }
