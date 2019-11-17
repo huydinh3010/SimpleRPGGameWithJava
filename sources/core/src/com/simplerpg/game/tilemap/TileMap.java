32c5289d62c8362dcc73c5e53caf3d326935bf5c
Thế Anh Hoàng <anhhtchy@gmail.com>
2019-11-17 08:14:26 +0700

    Revert "Revert "Merge branch 'enemy' into menu""
    This reverts commit 89aa910ea62ff47a6833dc346c4708fa8c2f0772.

--- sources/core/src/com/simplerpg/game/tilemap/TileMap.java
+++ sources/core/src/com/simplerpg/game/tilemap/TileMap.java
@@ -5,19 +5,23 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class TileMap {
     private TileSet items[][];
+    private int mapArray[][]; // luu lai mang truyen vao
     private int cellSize = 30;
+    // kich thuoc map theo pixels
+    private int mapWidth;
+    private int mapHeight;
 
     public static final int[][] test = {
             {6,6,6,6,6,6,6,6,6,6},
+            {6,5,2,2,0,0,0,2,2,6},
             {6,2,2,2,2,2,2,2,2,6},
+            {6,2,2,2,1,2,2,5,2,6},
+            {6,2,2,2,1,2,2,5,2,6},
+            {6,2,2,2,1,2,2,5,2,6},
+            {6,2,2,2,2,2,2,4,2,6},
+            {6,2,5,5,5,2,2,4,2,6},
             {6,2,2,2,2,2,2,2,2,6},
-            {6,2,2,2,2,2,2,2,2,6},
-            {6,2,2,2,2,2,2,2,2,6},
-            {6,2,2,2,2,2,2,2,2,6},
-            {6,2,2,2,2,2,2,2,2,6},
-            {6,2,2,2,2,2,2,2,2,6},
-            {6,2,2,2,2,2,2,2,2,6},
-            {6,6,6,6,6,6,6,6,6,6},
+            {6,6,6,6,6,6,6,6,6,6}
     };
 
     public void loadMapFromFile(String mapFile){
@@ -25,6 +29,7 @@ public class TileMap {
     }
 
     public void loadMapFromArray(int[][] items){
+        this.mapArray = items;
         ItemTileSetType[][] itemTypes = new ItemTileSetType[items.length][items[0].length];
         ItemTileSetType[] mapping = {
                 ItemTileSetType.BUSH_BANANA,
@@ -49,16 +54,44 @@ public class TileMap {
                 items[i][j] = new TileSet(itemTypes[i][j]);
             }
         }
+        this.mapWidth = itemTypes.length * this.cellSize;
+        this.mapHeight = itemTypes[0].length * this.cellSize;
     }
 
     public void draw(SpriteBatch batch){
+        Sprite ground = new TileSet(ItemTileSetType.GROUND).getSprite();
         for(int i = 0; i < items.length; i++){
             for (int j = 0; j < items[0].length; j++){
-                Sprite sprite = items[i][j].getSprite();
-                sprite.setPosition(i * cellSize, j * cellSize);
-                sprite.draw(batch);
+                // luon ve GROUND truoc o duoi moi tile
+                ground.setPosition(i * cellSize, j * cellSize);
+                ground.draw(batch);
+                if (this.mapArray[i][j] != 2) { // neu khong phai GROUND moi can ve
+                    Sprite sprite = items[i][j].getSprite();
+                    sprite.setPosition(i * cellSize, j * cellSize);
+                    sprite.setSize(this.cellSize, this.cellSize);
+                    sprite.draw(batch);
+                }
             }
         }
+    }
+
+    public boolean hitAWall(float x, float y, int radius, int reduceR){
+        // kiem tra nhan vat tai vi tri (x, y), co ban kinh radius co cham vao tuong hay khong.
+        // reduceR la phan ban kinh giam di de nhan vat co the lot qua cac khoang hep.
+        float end_x = x + 2 * radius;
+        float end_y = y + 2 * radius;
+        int start_i = (int) ((x+reduceR) / this.cellSize);
+        int end_i = (int) ((end_x-reduceR) / this.cellSize);
+        int start_j = (int) ((y+reduceR) / this.cellSize);
+        int end_j = (int) ((end_y-reduceR) / this.cellSize);
+        for (int i = start_i; i <= end_i; i++){
+            for (int j = start_j; j <= end_j; j++){
+                if (this.mapArray[i][j] != 2){ // khong di duoc vao tile khac voi GROUND
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     public TileSet[][] getItems() {
@@ -76,4 +109,8 @@ public class TileMap {
     public void setCellSize(int cellSize) {
         this.cellSize = cellSize;
     }
+
+    public int getMapWidth() { return mapWidth; }
+
+    public int getMapHeight() { return mapHeight; }
 }
